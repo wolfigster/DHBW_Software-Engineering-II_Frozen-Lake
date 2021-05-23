24be5a670a8d0eed466fe38ece6a722a89899651
Johannes Weis <joh.weis.18@lehre.mosbach.dhbw.de>
2021-05-23 12:35:49 +0200

    add Lake and Field

---
+++ src/main/java/base/Field.java
@@ -0,0 +1,43 @@
+package base;
+
+import java.util.HashMap;
+
+public enum Field {
+    START("Start", 'S', -1),
+    HOLE("Hole", 'H', -1),
+    FIELD("Field", 'F', 0),
+    GOAL("Goal", 'G', 1);
+
+    private static final HashMap<Character, Field> FIELDS = new HashMap<>();
+    private final String name;
+    private final char label;
+    private final int reward;
+
+    static {
+        for (Field field : values()) {
+            FIELDS.put(field.label, field);
+        }
+    }
+
+    Field(String name, Character label, int reward) {
+        this.name = name;
+        this.label = label;
+        this.reward = reward;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public char getLabel() {
+        return label;
+    }
+
+    public int getReward() {
+        return reward;
+    }
+
+    public static Field getFieldByLabel(char label) {
+        return FIELDS.get(label);
+    }
+}
---
+++ src/main/java/base/Lake.java
@@ -0,0 +1,67 @@
+package base;
+
+import configuration.Configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Lake {
+    private Field[][] lake;
+
+    public Lake(String data) {
+        try (final FileInputStream fileInputStream = new FileInputStream(data)) {
+            // temporary arraylist fields to read in all chars
+            ArrayList<Field> fields = new ArrayList<>();
+            int content, height = 1;
+            // read file char by char and only accept
+            while ((content = fileInputStream.read()) != -1) {
+                final char c = (char) content;
+                // check for linebreak and increment height
+                if (c == '\n') height++;
+                // continue while if char is not a regular field
+                if (Field.getFieldByLabel(c) == null) continue;
+                fields.add(Field.getFieldByLabel(c));
+            }
+
+            // set lake dimensions
+            Configuration.instance.setLakeSize(height, fields.size() / height);
+            lake = new Field[Configuration.instance.lakeHeight][Configuration.instance.lakeWidth];
+            int n = 0;
+            // initialize two dimensional lake array
+            for (int y = 0; y < Configuration.instance.lakeHeight; y++) {
+                for (int x = 0; x < Configuration.instance.lakeWidth; x++) {
+                    lake[y][x] = fields.get(n);
+                    n++;
+                }
+            }
+        } catch (final IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Field[][] getLake() {
+        return lake;
+    }
+
+    public void printLake() {
+        for (int y = 0; y < Configuration.instance.lakeHeight; y++) {
+            for (int x = 0; x < Configuration.instance.lakeWidth; x++) {
+                System.out.print(lake[y][x].getLabel() + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    // method to return lake as one dimensional array to write it to the log file
+    public String[] getLakeAsStringArray() {
+        String[] lakeString = new String[Configuration.instance.lakeHeight];
+        for (int y = 0; y < Configuration.instance.lakeHeight; y++) {
+            lakeString[y] = "";
+            for (int x = 0; x < Configuration.instance.lakeWidth; x++) {
+                lakeString[y] += lake[y][x].getLabel() + " ";
+            }
+        }
+        return lakeString;
+    }
+}
